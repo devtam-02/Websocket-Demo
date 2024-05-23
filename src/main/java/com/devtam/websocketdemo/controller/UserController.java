bc996c2cd96b545d90a3d560345a004b20ca8a5a
TamNTTc <TamNTTc@bkav.com>
2024-05-23 11:41:36 +0700

    devtam-02: hehe

--- src/main/java/com/devtam/websocketdemo/controller/UserController.java
+++ src/main/java/com/devtam/websocketdemo/controller/UserController.java
@@ -3,19 +3,35 @@ package com.devtam.websocketdemo.controller;
 import com.devtam.websocketdemo.service.UserService;
 import com.devtam.websocketdemo.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
     @MessageMapping("/user.addUser")
     @SendTo("/user/topic")
     public User addUser(@Payload User user){
         userService.saveUser(user);
         return user;
     }
+
+    @MessageMapping("/user.disconnectUser")
+    @SendTo("/user/topic")
+    public User disconnect(@Payload User user){
+        userService.disconnect(user);
+        return user;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> findConnectedUser(){
+        return ResponseEntity.ok(userService.findConnectedUsers());
+    }
+
 }
